0a0372ea4c112f8baa36fdec15ae3f0098a1bfc7
Jeff Stinson <jeffstinson@hotmail.com>
2025-01-09 22:59:30 -0800

    create api call

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 # app.py
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
@@ -24,4 +26,4 @@ def post_data():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
