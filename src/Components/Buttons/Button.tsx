0dea35f40a4e98cc0a3b5998407935fcb9f76eaa
Jeff Stinson <jeffstinson@hotmail.com>
2025-01-14 22:58:50 -0800

    add docstrings and more props to Button component

--- src/Components/Buttons/Button.tsx
+++ src/Components/Buttons/Button.tsx
@@ -2,19 +2,37 @@ import React from "react";
 import { ButtonProps, Button as MuiButton } from "@mui/material";
 
 interface IButtonProps extends ButtonProps {
-	text: string;
-	onClick: () => void;
+	/** The text or React node to display in the button */
+	children: string | React.ReactNode;
+	/** The function to call when the button is clicked */
+	onClick?: () => void;
 }
 
-const Button: React.FC<IButtonProps> = ({ text, onClick, sx }) => {
+/**
+ * A generic button component with a custom style.
+ * @param {IButtonProps} props - The props for the button component.
+ * @returns {JSX.Element} - The button component.
+ */
+const Button: React.FC<IButtonProps> = ({
+	children,
+	onClick,
+	sx,
+	...buttonProps
+}) => {
 	return (
 		<MuiButton
 			variant="contained"
 			color="primary"
 			onClick={onClick}
-			sx={{ borderRadius: "2px", ...sx }}
+			sx={{
+				borderRadius: "2px",
+				background: "#c79f27",
+				color: "#ffffff",
+				...sx,
+			}}
+			{...buttonProps}
 		>
-			{text}
+			{children}
 		</MuiButton>
 	);
 };
