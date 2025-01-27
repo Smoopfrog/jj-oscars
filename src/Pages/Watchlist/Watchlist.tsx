3c350ba985768e493ac3de6fbaf55cbfdbb1b9d8
Jeff Stinson <jeffstinson@hotmail.com>
2025-01-26 23:28:05 -0800

    add formik to watchlist

--- src/Pages/Watchlist/Watchlist.tsx
+++ src/Pages/Watchlist/Watchlist.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import TopNav from "../../Components/TopNav/TopNav";
 import { categories } from "../../Data/Nominees";
 import NominatedMovie from "./NominatedMovie";
+import { Formik } from "formik";
 
 export interface IMovieCategory {
 	category: string;
@@ -30,6 +31,14 @@ const Watchlist = () => {
 		});
 	});
 
+	const handleSubmit = (values: any) => {
+		console.log(values);
+	};
+
+	const handleCheckboxChange = (movie: string, event: any) => {
+		handleSubmit({ [movie]: event.target.checked });
+	};
+
 	return (
 		<Box sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
 			<TopNav />
@@ -41,23 +50,24 @@ const Watchlist = () => {
 					alignItems: "center",
 				}}
 			>
-				<Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-					{Object.entries(movies)
-						.sort((a, b) => a[0].localeCompare(b[0]))
-						.map(([movie, categories]) => (
-							<FormControlLabel
-								control={<Checkbox color="default" />}
-								label={
-									<NominatedMovie
-										key={movie}
-										movie={movie}
-										categories={categories}
-									/>
-								}
-								sx={{ alignItems: "flex-start" }}
-							/>
-						))}
-				</Box>
+				<Formik initialValues={{}} onSubmit={handleSubmit}>
+					<Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+						{Object.entries(movies)
+							.sort((a, b) => a[0].localeCompare(b[0]))
+							.map(([movie, categories], i) => (
+								<FormControlLabel
+									key={i}
+									control={<Checkbox color="default" />}
+									value={movie}
+									label={
+										<NominatedMovie movie={movie} categories={categories} />
+									}
+									onChange={(e) => handleCheckboxChange(movie, e)}
+									sx={{ alignItems: "flex-start" }}
+								/>
+							))}
+					</Box>
+				</Formik>
 			</Box>
 		</Box>
 	);
