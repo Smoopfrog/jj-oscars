6a616f55e1d434134b681d37ffcab2ec026e5ba2
Jeff Stinson <jeffstinson@hotmail.com>
2025-02-20 00:29:51 -0800

    add year selector to battle table

--- src/Pages/Battle/Battle.tsx
+++ src/Pages/Battle/Battle.tsx
@@ -1,8 +1,18 @@
-import { Box, Table, Typography } from "@mui/material";
-import React from "react";
+import {
+	Box,
+	MenuItem,
+	Select,
+	SelectChangeEvent,
+	Table,
+	Typography,
+} from "@mui/material";
+import React, { useState } from "react";
 import BattleTable from "./BattleTable";
+import dayjs from "dayjs";
 
 const Battle = () => {
+	const [year, setYear] = useState<number>(dayjs().year());
+
 	return (
 		<Box
 			display={"flex"}
@@ -11,7 +21,39 @@ const Battle = () => {
 			gap={2}
 			mb={5}
 		>
-			<Typography variant="h4">Oscar Battle 2025</Typography>
+			<Box
+				display={"flex"}
+				gap={1}
+				alignItems={"center"}
+				justifyContent={"space-between"}
+			>
+				<Typography variant="h4">Oscar Battle </Typography>
+				<Select
+					labelId="year-select-label"
+					id="year-select"
+					value={year}
+					label={
+						<Typography variant="h4" fontSize={34}>
+							{year}
+						</Typography>
+					}
+					variant="standard"
+					onChange={(e: SelectChangeEvent<number>) =>
+						setYear(e.target.value as number)
+					}
+					sx={{
+						padding: 0,
+						fontSize: 34,
+						"& .MuiSelect-select": {
+							padding: 0,
+						},
+					}}
+				>
+					<MenuItem value={2025}>2025</MenuItem>
+					<MenuItem value={2024}>2024</MenuItem>
+					<MenuItem value={2023}>2023</MenuItem>
+				</Select>
+			</Box>
 			<BattleTable />
 		</Box>
 	);
