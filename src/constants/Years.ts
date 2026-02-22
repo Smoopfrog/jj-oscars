a521e849f29339eead73a03dbf9e681c7aac1a10
Jeff Stinson <jeffstinson@hotmail.com>
2026-02-22 14:17:53 -0800

    Update years constant to include 2026 for upcoming events

--- src/constants/Years.ts
+++ src/constants/Years.ts
@@ -1,3 +1,3 @@
-export const years: number[] = [2025, 2024, 2023, 2022];
+export const years: number[] = [2026, 2025, 2024, 2023, 2022];
 
 export const yearsString: string[] = years.map((year) => year.toString());
